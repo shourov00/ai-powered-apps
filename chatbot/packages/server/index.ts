e599bbe3fea4aba72ef36fd613414854361453c3
Fazley Rabbi <sfazleyrabbi@gmail.com>
2025-09-13 16:20:50 +0600

    set up server with Express and dotenv, add initial configuration files

---
+++ chatbot/packages/server/index.ts
@@ -0,0 +1,16 @@
+import type {Request, Response} from 'express'
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send("Hello from the server!")
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
