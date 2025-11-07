11beae48e3910274c50ef61f019878894f795bad
Fazley Rabbi <sfazleyrabbi@gmail.com>
2025-11-07 11:23:11 +0600

    update server port to 3000 for local development

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(router)
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
